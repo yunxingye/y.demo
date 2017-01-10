6682495d00a978752cb007c8eca6e3bf1c67c44f
yunxing <abusedna@gmail.com>
2017-01-10 11:15:31 -0800

    initial commit

---
+++ test_github/test_github.cpp
@@ -0,0 +1,13 @@
+// test_github.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	getchar();
+    return 0;
+}
+
